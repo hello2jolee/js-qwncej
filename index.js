03ec879592922ec2e01c3d7c83499f886d592c9c
hello2jolee <jioun.lee.dev@gmail.com>
2021-02-16 21:19:28 +0900

    Infinite Scroll

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import "./style.css";
 import renderList from "./listRenderer";
+import { debounce } from "./util";
 
 const app = document.querySelector("#app");
 const fetchMoreTrigger = document.querySelector("#fetchMore");
@@ -12,9 +13,25 @@ const fetchMore = async () => {
   target.classList.remove("loading");
 };
 
+// 매번 스크롤을 발생시킬 경우 성능 저하가 발생할 수 있기 때문에 중간에 한 번 확인 || 마지막에 한 번 확인하는 방법이 적절
 const onScroll = e => {
   // do something (hint: e.target.scrollingElement)
+  const { scrollHeight, scrollTop, clientHeight } = e.target.scrollingElement;
+
+  if (scrollTop + clientHeight === scrollHeight) {
+    fetchMore();
+  }
 };
 
-document.addEventListener("scroll", onScroll);
+// throttle: 일정시간간격으로 한 번씩만 실행
+// debounce: 연속으로 발생하는 이벤트에 대해 마지막 한 번만 실행
+
+document.addEventListener("scroll", debounce(onScroll, 300));
 fetchMore();
+
+// https://developer.mozilla.org/ko/docs/Web/API/IntersectionObserver/IntersectionObserver
+// const fetchMoreObserver = new IntersectionObserver(([{ isIntersecting }]) => {
+//   if (isIntersecting) fetchMore();
+// });
+// fetchMoreObserver.observe(fetchMoreTrigger);
+// fetchMore();
--- util.js
+++ util.js
@@ -6,3 +6,11 @@ export const randomTimer = (func, ...args) => resolve => {
 
 export const dummyFetcher = (method, args) =>
   new Promise(randomTimer(method, args));
+
+export const debounce = (func, delay) => {
+  let timeoutId = null;
+  return (...args) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(func.bind(null, ...args), delay);
+  };
+};
